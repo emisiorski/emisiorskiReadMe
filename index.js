ad9a6a48f651daad647ad482c7433cdfb8c75472
Erica <ericamisiorski@gmail.com>
2021-11-28 18:25:12 -0500

    add to markdown

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // TODO: Include packages needed for this application
-
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
+// const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
+const questions = [
         {
             type: 'input',
             name: 'github name',
@@ -20,7 +19,18 @@ const questions = () => {
         {
             type: 'input',
             name: 'Description',
-            message: 'Enter a description for your project'
+            message: 'Enter a description for your project',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("You must enter a description for your project.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'Table of Contents',
+            message: 'Table of Contents'
         },
         {
             type: 'input',
@@ -35,7 +45,7 @@ const questions = () => {
         {
             type: 'input',
             name: 'contribution',
-            message: "Enter contribution guidelines for your project:"
+            message: "Enter contributors to your project:"
         },
         {
             type: 'input',
@@ -43,34 +53,45 @@ const questions = () => {
             message: "Enter test information for your project:"
         },
         {
-            type: 'input',
-            name: 'testing',
-            message: "Enter test information for your project:",
+            type: 'list',
+            name: 'license',
+            message: "Choose License information for your project (if there is a license)",
+            choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib'],
         },
         {
             type: "input",
-            message: "Enter your email address:",
+            message: "Type any questions you have",
+            name: "questions"
+        },
+        {
+            type: "input",
+            message: "Contact info for inquiry (email address)",
             name: "email"
         }
-    ])
-};
+
+    ]
+
+
 
 // TODO: Create a function to write README file
+
+
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
         console.log(answers)
         var template = generateMarkdown(answers)
-        console.log(template)
-        fs.writeFile("README.md", template, (error) => {
-            if (error) {
-                console.log(error)
-            } else {
-                console.log("successfully created")
-            }
-        })
+    fs.writeFile ('./README.md'), template, (error) => {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log("successfully created")
+        }
     })
-}
+})
+    }
+    
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const inquirer = require("inquirer");
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
 
+  ## Description:
+  ${answers.description}
+
+  ## Table of Contents
+  - Description (#description)
+  - Installation (#installation)
+  - Usage (#usage)
+  - Contributors (#contribution)
+  - Tests (#testing)
+  - Questions (#questions)
+
+  ## Installation:
+  ${answers.installation}
+
+  ## Usage:
+  ${answers.usage}
+
+  ## Contributors:
+  ${answers.contribution}
+
+  ## Tests:
+  ${answers.testing}
+
+  ## Questions:
+  ${answers.questions}
 `;
 }
 
